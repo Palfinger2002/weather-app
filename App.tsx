b62cebedd942a94e3cf596dba7adf6505ae29b69
Nazar <nazar.regalov@gmail.com>
2026-07-30 12:18:17 +0300

    Show humidity and wind speed

--- App.tsx
+++ App.tsx
@@ -4,6 +4,8 @@ import { StyleSheet, Text, View } from "react-native";
 interface WeatherResponse {
   current: {
     temperature_2m: number;
+    relative_humidity_2m: number;
+    wind_speed_10m: number;
   };
 }
 
@@ -11,7 +13,7 @@ export default function App() {
   const [weather, setWeather] = useState<WeatherResponse | null>(null);
 
   const url =
-    "https://api.open-meteo.com/v1/forecast?latitude=50.45&longitude=30.52&current=temperature_2m";
+    "https://api.open-meteo.com/v1/forecast?latitude=50.45&longitude=30.52&current=temperature_2m,relative_humidity_2m,wind_speed_10m";
 
   useEffect(() => {
     fetch(url)
@@ -21,9 +23,17 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>
-        {weather === null
-          ? "Loading..."
-          : `Температура: ${weather?.current?.temperature_2m}C`}
+        {weather === null ? (
+          <Text>Loading...</Text>
+        ) : (
+          <>
+            <Text>Температура: {weather?.current?.temperature_2m}°C</Text>
+            <Text>
+              Відносна вологість: {weather?.current?.relative_humidity_2m}
+            </Text>
+            <Text>Швидкість вітру: {weather?.current?.wind_speed_10m}</Text>
+          </>
+        )}
       </Text>
     </View>
   );
