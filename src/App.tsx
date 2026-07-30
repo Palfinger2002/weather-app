d0cbffa2af8f9db47da3ea56fbbca34de5ef7fd9
Nazar <nazar.regalov@gmail.com>
2026-07-30 15:12:39 +0300

    Add temperature, location and metrics row layout

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text>
+      {/* <Text>
         {weather === null ? (
           <Text>{error !== null ? error : "Loading..."}</Text>
         ) : (
@@ -18,7 +18,35 @@ export default function App() {
             <Text>Швидкість вітру: {weather?.current?.wind_speed_10m}</Text>
           </>
         )}
-      </Text>
+      </Text> */}
+
+      <View style={{ flexDirection: "row" }}>
+        <Text style={styles.bigTemp}>{weather?.current?.temperature_2m}°</Text>
+        <Text style={styles.locationText}> Party Cloudy</Text>
+      </View>
+      <View>
+        <Text style={styles.locationText}>South Jakarta, Indonesia ▾</Text>
+      </View>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-around",
+          width: "100%",
+        }}
+      >
+        <View>
+          <Text>{weather?.current?.relative_humidity_2m}%</Text>
+          <Text>Humidity</Text>
+        </View>
+        <View>
+          <Text>11</Text>
+          <Text>UV Index</Text>
+        </View>
+        <View>
+          <Text>E {weather?.current?.wind_speed_10m} kmh</Text>
+          <Text>Wind</Text>
+        </View>
+      </View>
     </View>
   );
 }
@@ -29,5 +57,19 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     paddingTop: 10,
+  },
+
+  bigTemp: {
+    fontSize: 42,
+    lineHeight: 44,
+    letterSpacing: 0,
+    fontWeight: 400,
+  },
+
+  locationText: {
+    fontSize: 12,
+    lineHeight: 20,
+    letterSpacing: 0,
+    fontWeight: 400,
   },
 });
